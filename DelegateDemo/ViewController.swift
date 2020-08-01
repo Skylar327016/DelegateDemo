22161673cf9ff891cd4ccb763b41082e8ba05686
Skylar <apa6143171@gmail.com>
2020-08-01 09:59:13 +0800

    Initial Commit

---
+++ DelegateDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateDemo
+//
+//  Created by 陳家豪 on 2020/8/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
